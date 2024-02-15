d9d688819aa0e21046a72544faa68f1137588641
Eric Batiste <ericbatiste4@gmail.com>
2024-02-15 14:10:29 -0700

    Feat: add NavLinks to header, install Router.

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,7 +1,27 @@
 import './App.css';
+import { NavLink, Routes, Route } from 'react-router-dom';
 
 function App() {
-  return <h1>Quintessential Cocktails</h1>;
+  return (
+    <main className="App">
+      <header>
+        <h1>Quintessential Cocktails</h1>;
+        <nav>
+          <NavLink to='/coctailshome' className='nav-link'>Home</NavLink>
+          <NavLink to='/randomcocktail' className='nav-link'>Random Cocktail</NavLink>
+          <NavLink to='/favorites' className='nav-link'></NavLink>
+          <NavLink to='/' className='nav-link'>Sign Out</NavLink>
+        </nav>
+      </header>
+      <Routes>
+        <Route></Route>
+        <Route></Route>
+        <Route></Route>
+        <Route></Route>
+        <Route></Route>
+      </Routes>
+    </main>
+  )  
 }
 
 export default App;
