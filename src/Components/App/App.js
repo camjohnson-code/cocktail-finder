a07d0ad46500da84e4659f50a9831b7d975f920d
Eric Batiste <ericbatiste4@gmail.com>
2024-02-15 14:36:30 -0700

    Style: styled header /Nav.

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,11 +5,11 @@ function App() {
   return (
     <main className="App">
       <header>
-        <h1>Quintessential Cocktails</h1>;
+        <h1>Quintessential Cocktails</h1>
         <nav>
           <NavLink to='/coctailshome' className='nav-link'>Home</NavLink>
           <NavLink to='/randomcocktail' className='nav-link'>Random Cocktail</NavLink>
-          <NavLink to='/favorites' className='nav-link'></NavLink>
+          <NavLink to='/favorites' className='nav-link'>Favorites</NavLink>
           <NavLink to='/' className='nav-link'>Sign Out</NavLink>
         </nav>
       </header>
