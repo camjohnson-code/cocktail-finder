3c55bc97f7c6a6d0d4bc9b7e7b0ce445955d4355
Eric Batiste <ericbatiste4@gmail.com>
2024-02-16 08:55:20 -0700

    Feat: refactor listIngredients() function to handle null data.

--- src/Components/Details/Details.js
+++ src/Components/Details/Details.js
@@ -7,10 +7,12 @@ const Details = () => {
     return Object.entries(mockDetails).reduceRight((a, c) => {
       const num = c[0].length - 1;
       const key = c[0][num];
-      if (c[0].includes('Ingredient') || c[0].includes('Measure')) {
-        a[key] = a[key] || [];
-        if (a[key]) a[key].push(c[1]);
-      }
+      if (c[1] !== null) {
+        if (c[0].includes('Ingredient') || c[0].includes('Measure')) {
+          a[key] = a[key] || [];
+          if (a[key] !== null) a[key].push(c[1]);
+        }
+      } 
       return a
     }, {})
   }
