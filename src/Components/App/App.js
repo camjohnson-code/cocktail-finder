ab3c268d8328bc657d50ec278ff7616c36f1d569
Cam Johnson <cameron.n.johnson15@gmail.com>
2024-02-15 20:39:04 -0700

    Add SearchPage component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,34 +2,35 @@ import './App.css';
 import React, { useState } from 'react';
 import { NavLink, Routes, Route } from 'react-router-dom';
 import LogInPage from '../Log In Page/LogInPage';
+import SearchPage from '../Search Page/SearchPage';
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
 
   return (
-    <main className="App">
+    <main className='App'>
       {isLoggedIn && (
         <header>
           <h1>Quintessential Cocktails</h1>
           <nav>
-            <NavLink to="/coctailshome" className="nav-link">
+            <NavLink to='/coctailshome' className='nav-link'>
               Home
             </NavLink>
-            <NavLink to="/randomcocktail" className="nav-link">
+            <NavLink to='/randomcocktail' className='nav-link'>
               Random Cocktail
             </NavLink>
-            <NavLink to="/favorites" className="nav-link">
+            <NavLink to='/favorites' className='nav-link'>
               Favorites
             </NavLink>
-            <NavLink to="/" className="nav-link">
+            <NavLink to='/' className='nav-link'>
               Sign Out
             </NavLink>
           </nav>
         </header>
       )}
       <Routes>
-        <Route path="/" element={<LogInPage />}></Route>
-        <Route></Route>
+        <Route path='/' element={<LogInPage />}></Route>
+        <Route path='/home' element={<SearchPage />}></Route>
         <Route></Route>
         <Route></Route>
         <Route></Route>
