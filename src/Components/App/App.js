e8216ce27fd77158662f3942b292720347170be3
Cam Johnson <cameron.n.johnson15@gmail.com>
2024-02-18 17:31:39 -0700

    Update route for random cocktails page

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -6,34 +6,63 @@ import LogInPage from '../Log In Page/LogInPage';
 import Details from '../Details/Details';
 import SearchPage from '../Search Page/SearchPage';
 import NotFoundPage from '../Not Found Page/NotFoundPage';
+import RandomCocktailPage from '../Random Cocktail Page/RandomCocktailPage';
 
 function App() {
   const location = useLocation();
   const navigate = useNavigate();
-  const [isLoggedIn, setIsLoggedIn] = useState(localStorage.getItem('isLoggedIn') === 'true');
+  const [isLoggedIn, setIsLoggedIn] = useState(
+    localStorage.getItem('isLoggedIn') === 'true'
+  );
 
   const isValidRoute = () => {
-    const validRoutes = ['/', '/cocktailshome'];
-    return validRoutes.includes(location.pathname) || location.pathname.startsWith('/details/');
+    const validRoutes = ['/', '/cocktailshome', '/randomcocktail'];
+    return (
+      validRoutes.includes(location.pathname) ||
+      location.pathname.startsWith('/details/')
+    );
   };
 
   return (
-    <main className="App">
+    <main className='App'>
       {isLoggedIn && isValidRoute() && (
         <Nav setIsLoggedIn={setIsLoggedIn} navigate={navigate}></Nav>
       )}
       <Routes>
-        <Route path="/" element={<LogInPage setIsLoggedIn={setIsLoggedIn} />}></Route>
         <Route
-          path="/cocktailshome"
-          element={isLoggedIn ? <SearchPage /> : <NotFoundPage isLoggedIn={isLoggedIn} />}
+          path='/'
+          element={<LogInPage setIsLoggedIn={setIsLoggedIn} />}
         ></Route>
         <Route
-          path="/details/:id"
-          element={isLoggedIn ? <Details /> : <NotFoundPage isLoggedIn={isLoggedIn} />}
+          path='/cocktailshome'
+          element={
+            isLoggedIn ? (
+              <SearchPage />
+            ) : (
+              <NotFoundPage isLoggedIn={isLoggedIn} />
+            )
+          }
         ></Route>
-        <Route></Route>
-        <Route path="*" element={<NotFoundPage isLoggedIn={isLoggedIn} />}></Route>
+        <Route
+          path='/details/:id'
+          element={
+            isLoggedIn ? <Details /> : <NotFoundPage isLoggedIn={isLoggedIn} />
+          }
+        ></Route>
+        <Route
+          path='/randomcocktail'
+          element={
+            isLoggedIn ? (
+              <RandomCocktailPage />
+            ) : (
+              <NotFoundPage isLoggedIn={isLoggedIn} />
+            )
+          }
+        ></Route>
+        <Route
+          path='*'
+          element={<NotFoundPage isLoggedIn={isLoggedIn} />}
+        ></Route>
       </Routes>
     </main>
   );
