4edb8f5fd4ad7e6f7c3b238453221b521406d062
Cam Johnson <cameron.n.johnson15@gmail.com>
2024-02-19 11:06:35 -0700

    Refactor AddToFavorites to prevent duplicates

--- src/Components/Details/Details.js
+++ src/Components/Details/Details.js
@@ -6,11 +6,22 @@ import { getCocktailDetails } from '../../ApiCalls';
 const Details = ({ favorites, setFavorites }) => {
   const [selectedCocktail, setSelectedCocktail] = useState(null);
   const [error, setError] = useState('');
+  const [buttonText, setButtonText] = useState('Add to Favorites');
   const id = useParams().id;
 
+  const faves = JSON.parse(localStorage.getItem('stored-faves'));
+
   const addToFavorites = () => {
-    setFavorites([...favorites, selectedCocktail])
-  }
+    const alreadyInFavorites = favorites.some(
+      (drink) => drink.idDrink === selectedCocktail.idDrink
+    );
+
+    if (!alreadyInFavorites) {
+      const updatedFavorites = [...favorites, selectedCocktail];
+      setFavorites(updatedFavorites);
+      setButtonText('Added!');
+    }
+  };
 
   useEffect(() => {
     getCocktailDetails(id)
@@ -68,7 +79,7 @@ const Details = ({ favorites, setFavorites }) => {
         <p>Contemporary Classics</p>
         <img src={selectedCocktail.strDrinkThumb} alt=''></img>
       </hgroup>
-      <button onClick={() => addToFavorites()}>Add to Favorites</button> 
+      <button onClick={() => addToFavorites()}>{buttonText}</button>
     </section>
   );
 };
