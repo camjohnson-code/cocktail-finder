63561d0f9bccfbd6a688749d1d2110ae9f8a39a4
Cam Johnson <cameron.n.johnson15@gmail.com>
2024-02-18 17:31:23 -0700

    Add getRandomCocktail function

--- src/ApiCalls.js
+++ src/ApiCalls.js
@@ -5,12 +5,23 @@ export const getCocktail = (drink) => {
 };
 
 export const getCocktailDetails = (id) => {
-  return fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`).then(
-    (res) => {
-      if (!res.ok) {
-        throw new Error('Something went wrong, try refreshing the page.');
-      }
-      return res.json();
+  return fetch(
+    `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`
+  ).then((res) => {
+    if (!res.ok) {
+      throw new Error('Something went wrong, try refreshing the page.');
     }
-  );
+    return res.json();
+  });
+};
+
+export const getRandomCocktail = (spirit) => {
+  return fetch(
+    `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${spirit}`
+  ).then((res) => {
+    if (!res.ok) {
+      throw new Error('Something went wrong, try refreshing the page.');
+    }
+    return res.json();
+  });
 };
